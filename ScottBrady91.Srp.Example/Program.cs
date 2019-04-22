c95fd61c73b52e195ee352ce3d94007a725f5736
Scott Brady <scott@scottbrady91.com>
2019-04-22 13:50:56 +0100

    Project init

---
+++ ScottBrady91.Srp.Example/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ScottBrady91.Srp.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string username = "alice"; // I - user username
+            const string password = "password123"; // P - user password
+            const string saltHex = "BEB25379D1A8581EB5A727673A2441EE"; // s - user salt
+
+
+
+        }
+
+        
+    }
+}
---
+++ ScottBrady91.Srp.Example/SrpClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScottBrady91.Srp.Example
+{
+    public class SrpClient
+    {
+        public string GenerateVerifier()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
